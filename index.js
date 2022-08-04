07889c80f104ca5c641e458319e873a655523dba
Alex Basystyi <alexbasisty@gmail.com>
2022-08-04 12:58:03 +0200

    overwriting form method to delete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
+
 const { clientRouter } = require('./routers/client');
 const { homeRouter } = require('./routers/home');
 const { db } = require('./utils/db');
@@ -11,6 +13,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.static('public'));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.engine('.hbs', engine({
     extname: '.hbs',
