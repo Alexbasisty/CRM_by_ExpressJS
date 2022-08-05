aab93adf4679940576e65df48e2700af7e691222
Alex Basystyi <alexbasisty@gmail.com>
2022-08-05 13:03:22 +0200

    validating errors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const methodOverride = require('method-override');
 
 const { clientRouter } = require('./routers/client');
 const { homeRouter } = require('./routers/home');
-const { db } = require('./utils/db');
+const { handleError } = require('./utils/errors');
 
 const app = express();
 
@@ -22,6 +22,8 @@ app.set('view engine', '.hbs');
 
 app.use('/', homeRouter);
 app.use('/client', clientRouter);
+
+app.use(handleError);
 
 app.listen(3000, 'localhost', () => {
     console.log('Localhost is ON, adress => http://localhost:3000/');
